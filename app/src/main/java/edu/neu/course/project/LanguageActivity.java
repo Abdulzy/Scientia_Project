8fb1329a465ce6a1c9b6464d6cb066c6483c4a99
MeeraSurendran <meera02712@gmail.com>
2021-12-09 12:19:52 -0500

    Fixed progress dialog

--- app/src/main/java/edu/neu/course/project/LanguageActivity.java
+++ app/src/main/java/edu/neu/course/project/LanguageActivity.java
@@ -5,30 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class LanguageActivity extends AppCompatActivity {
+public class LanguageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Spinner knownLanguage;
     Spinner learnLanguage;
     ArrayAdapter<String> adapter;
+    private String selectedKnownLanguage;
+    private String selectedLearnLanguage;
+    private Spinner spinner1;
+    private Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
-        knownLanguage = findViewById(R.id.knownLanguage);
-        learnLanguage = findViewById(R.id.learnLanguage);
-
-        adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.languages));
+        spinner1 = findViewById(R.id.knownLanguage);
+        spinner2 = findViewById(R.id.learnLanguage);
+        adapter = new ArrayAdapter<String>(LanguageActivity.this,
+                android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.languages));
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        knownLanguage.setAdapter(adapter);
-        learnLanguage.setAdapter(adapter);
+        spinner1.setAdapter(adapter);
+        spinner2.setAdapter(adapter);
+        spinner1.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
     }
+
     public void dash(View view) {
         Intent intent = new Intent(this, DashBoard.class);
+        intent.putExtra("user", "Meera");
+        intent.putExtra("chosenKnownLanguage", selectedKnownLanguage);
+        intent.putExtra("chosenLearnLanguage", selectedKnownLanguage);
         startActivity(intent);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (adapterView.getId() == R.id.knownLanguage) {
+             selectedKnownLanguage = adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(this, "&&&& " + selectedKnownLanguage, Toast.LENGTH_SHORT).show();
+        }
+
+        if (adapterView.getId() == R.id.learnLanguage) {
+             selectedLearnLanguage = adapterView.getItemAtPosition(i).toString();
+            Toast.makeText(this, "&&&& " + selectedLearnLanguage, Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
