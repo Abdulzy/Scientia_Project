67458f72cd897cc8b911d8c46f5da49814586ffd
abdul <abdul.isegen@gmail.com>
2021-12-12 17:09:28 -0500

    added change for tts

--- app/src/main/java/edu/neu/course/project/MainMenu.java
+++ app/src/main/java/edu/neu/course/project/MainMenu.java
@@ -8,30 +8,39 @@ import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
+
+    private String currentUser;
+    private String currentToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        Intent intent = getIntent();
+        currentUser = intent.getStringExtra("sender");
+        currentToken = intent.getStringExtra("token");
     }
 
 
     public void toPlay(View view) {
-        Intent intent = new Intent(MainMenu.this, Play.class);
+        Intent intent = new Intent(MainMenu.this, LanguageActivity.class);
+        intent.putExtra("sender", currentUser);
+        intent.putExtra("token", currentToken);
         startActivity(intent);
     }
 
     public void toLeaderboard(View view) {
         Intent intent = new Intent(MainMenu.this, LeaderBoard.class);
+        intent.putExtra("sender", currentUser);
+        intent.putExtra("token", currentToken);
         startActivity(intent);
     }
 
-    public void toLanguage(View view) {
-        Intent intent = new Intent(MainMenu.this, LanguageActivity.class);
-        startActivity(intent);
-    }
+
 
     public void toAboutUs(View view) {
         Intent intent = new Intent(MainMenu.this, AboutUs.class);
+        intent.putExtra("sender", currentUser);
+        intent.putExtra("token", currentToken);
         startActivity(intent);
     }
 }
